d00b01bfe7c106b894d261dfda6ccd701250f595
Sumo-99 <sumanthssr@gmail.com>
2020-08-11 11:15:16 +0530

    added build and node modules

---
+++ build/precache-manifest.8326a20b68763a0fa79b2982ce42cddb.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afb4682aa895f5db1e70bbccb35f626d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eeb4f534ad15eb91bb61",
+    "url": "/static/css/main.299202e1.chunk.css"
+  },
+  {
+    "revision": "75fbb90bfc05d5b3c835",
+    "url": "/static/js/2.e2be670e.chunk.js"
+  },
+  {
+    "revision": "253d0d72fd1c6eb1e9606c0cb98dca11",
+    "url": "/static/js/2.e2be670e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eeb4f534ad15eb91bb61",
+    "url": "/static/js/main.df2f995a.chunk.js"
+  },
+  {
+    "revision": "b873fba12ca2297e4f50",
+    "url": "/static/js/runtime-main.059249d9.js"
+  },
+  {
+    "revision": "43ff9a6eedd2d3dfc874513cd860dddb",
+    "url": "/static/media/down arrow.43ff9a6e.jpg"
+  },
+  {
+    "revision": "9635dfdb7b6aa8589c4119a661443b4c",
+    "url": "/static/media/homebg8.9635dfdb.png"
+  },
+  {
+    "revision": "5c24a99d6acdbcf08757792f228f6ba9",
+    "url": "/static/media/up arrow.5c24a99d.jpg"
+  }
+]);
